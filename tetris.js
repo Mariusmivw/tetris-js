9fb7dfe477c705a8677a5ca2630fb3b165065b49
Mariusmivw <mariusmivw@gmail.com>
2018-11-21 17:53:22 +0100

    cleanup

--- tetris.js
+++ tetris.js
@@ -1,34 +1,44 @@
-const drawGrid = async (grid) => {
-	for (let i = 0; i < currentTetrimino.shape.length; i++) {
-		for (let u = 0; u < currentTetrimino.shape.length; u++) {
-			let part = currentTetrimino.shape[i][u];
-			if (part != 0) {
-				grid[i + currentTetrimino.pos[0]][u + currentTetrimino.pos[1]] = part;
-			}
+const initGrid = () => {
+	let grid = [];
+	for (let i = 0; i < 20; i++) {
+		grid.push([]);
+		for (let u = 0; u < 10; u++) {
+			grid[i][u] = 0;
 		}
+	}
+	return grid;
+}
+
+const drawGrid = async (grid) => {
+	grid = $.extend(true, [], grid);
+
+	try {
+		currentTetrimino.shape.forEach((row, i) => {
+			row.forEach((cell, u) => {
+				if (cell != 0) {
+					grid[i + currentTetrimino.pos[0]][u + currentTetrimino.pos[1]] = cell;
+				}
+			});
+		});
+	} catch (e) {
+		pause();
+		throw e;
 	}
 
 	let html = "<let>let</let> <var>grid</var> = [";
-	for (const row of grid) {
+
+	grid.forEach(row => {
 		html += "[";
-		for (const item of row) {
-			html += `<t${item}/>,`;
-		}
+		row.forEach(cell => {
+			html += `<t${cell}/>,`;
+		});
 		html += `],<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`;
-	}
+	});
+
 	html += "];";
 	$("#game").html(
 		html.replace(/,\]/g, "]").replace(/\],<br>(?:&nbsp;)+\]/, "]]")
 	);
-
-	for (let i = 0; i < currentTetrimino.shape.length; i++) {
-		for (let u = 0; u < currentTetrimino.shape.length; u++) {
-			let part = currentTetrimino.shape[i][u];
-			if (part != 0) {
-				grid[i + currentTetrimino.pos[0]][u + currentTetrimino.pos[1]] = 0;
-			}
-		}
-	}
 };
 
 const pause = () => {
@@ -36,36 +46,7 @@ const pause = () => {
 	console.log("pause");
 }
 
-let grid = [
-	// [3, 4, 5, 6, 7, 0, 1, 2, 3, 4],
-	// [2, 3, 4, 5, 6, 7, 0, 1, 2, 3],
-	// [1, 2, 3, 4, 5, 6, 7, 0, 1, 2],
-	// [0, 1, 2, 3, 4, 5, 6, 7, 0, 1],
-	// [7, 0, 1, 2, 3, 4, 5, 6, 7, 0],
-	// [6, 7, 0, 1, 2, 3, 4, 5, 6, 7],
-	// [5, 6, 7, 0, 1, 2, 3, 4, 5, 6],
-	// [4, 5, 6, 7, 0, 1, 2, 3, 4, 5],
-	// [3, 4, 5, 6, 7, 0, 1, 2, 3, 4],
-	// [2, 3, 4, 5, 6, 7, 0, 1, 2, 3],
-	// [1, 2, 3, 4, 5, 6, 7, 0, 1, 2],
-	// [0, 1, 2, 3, 4, 5, 6, 7, 0, 1],
-	// [7, 0, 1, 2, 3, 4, 5, 6, 7, 0],
-	// [6, 7, 0, 1, 2, 3, 4, 5, 6, 7],
-	// [5, 6, 7, 0, 1, 2, 3, 4, 5, 6],
-	// [4, 5, 6, 7, 0, 1, 2, 3, 4, 5],
-	// [3, 4, 5, 6, 7, 0, 1, 2, 3, 4],
-	// [2, 3, 4, 5, 6, 7, 0, 1, 2, 3],
-	// [1, 2, 3, 4, 5, 6, 7, 0, 1, 2],
-	// [0, 1, 2, 3, 4, 5, 6, 7, 0, 1],
-];
-
-for (let i = 0; i < 20; i++) {
-	grid.push([]);
-	for (let u = 0; u < 10; u++) {
-		grid[i][u] = 0;
-	}
-}
-
+let grid = initGrid();
 let dropSpeed = 4;
 let currentTetrimino = new Tetrimino(Math.ceil(Math.random() * 7));
 let frameCount = 0;
